999eefa36b66860be2cce8eac13132c784779e7e
jessica-ralhan <jessicaralhan490@gmail.com>
2025-02-27 01:09:49 +0530

    data proccesing and logging

---
+++ data_processing/main.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+df = pd.DataFrame(
+    {
+        "Name" : [
+            "John Doe",
+            "Jane Smith",
+            "Alice Brown"
+        ],
+        "Department" : [
+            "Sales",
+            "Engineering",
+            "HR"
+        ],
+        "Email" : [
+            "john.doe@company.com",
+            "jane.smith@company.com",
+            "alice.brown@company.com"
+        ]
+    }
+)
+print(df)
+
